0037b642bd735e92dc6c7369c52427a9862d5f39
Michael Stenta <mike@mstenta.net>
2012-08-05 17:42:10 -0400

    Install essential development packages: build-essential, unzip, vim.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,11 @@ node 'default' {
   # Import settings.pp
   import 'settings'
 
+  # Install essential development packages.
+  package { ['build-essential', 'unzip', 'vim']:
+    ensure => present,
+  }
+
   # Install and configure Git.
   include git
   class { 'git::config':
