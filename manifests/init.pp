1334d5acd317bc5e724a144b962c313ec3a44c63
Michael Stenta <mike@mstenta.net>
2012-08-06 11:15:35 -0400

    Added third-party module for installing Google Chrome.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@ node 'default' {
   # Import settings.pp
   import 'settings'
 
+  # Define the apt-get update exec.
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    refreshonly => true,
+  }
+
   # Install essential development packages.
   package { ['build-essential', 'unzip', 'vim']:
     ensure => present,
@@ -15,4 +21,7 @@ node 'default' {
     name => $git_name,
     email => $git_email,
   }
+
+  # Install Chrome.
+  include google-chrome
 }
